f9a4832236dbe838ef93550b3c2fc41f0033bc53
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-09-21 19:50:51 -0300

    agregué filtro por radio

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const cards = document.querySelectorAll(".card")
 const botonesFiltro = document.querySelectorAll(".filtro-boton")
 const titulosTarjetas = document.querySelectorAll(".titulo")
 const filtro = document.getElementById("filtro-nombre")
+const filtroSexo = document.querySelectorAll("input[type='radio']")
+
+
+//armado de filtro por botones
 
 // for (let boton of botonesFiltro) {
 //     boton.onclick = () => {
@@ -21,14 +25,38 @@ const filtro = document.getElementById("filtro-nombre")
 
 //armado de filtro por input
 
-filtro.oninput = () =>{
-    for (let card of cards){
-        if(card.dataset.nombre.includes(filtro.value)){
-            card.classList.remove(`hidden`)
-        }
-        else {
-            card.classList.add(`hidden`)
-        }
-    }
+// filtro.oninput = () =>{
+//     for (let card of cards){
+//         if(card.dataset.nombre.includes(filtro.value.toLowerCase())){
+//             card.classList.remove(`hidden`)
+//         }
+//         else {
+//             card.classList.add(`hidden`)
+//         }
+//     }
+// }
+
+
+//armado de filtro por checkbox o radio
+
+for (let radio of filtroSexo){
+  radio.oninput = () => {
+      for (let card of cards) {
+          if (radio.value === "i"){
+              card.classList.remove (`hidden`)
+          }
+          else if (radio.checked) {
+              if (card.dataset.sexo === radio.value){
+                  card.classList.remove(`hidden`)
+              }
+              else {
+                  card.classList.add(`hidden`)
+              }
+          }
+          else {
+              card.classList.remove(`hidden`)
+          }
+      }
+  }
 }
 
