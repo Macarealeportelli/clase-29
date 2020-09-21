447fd8f173b8f9d7fff770047329cc16415f95b8
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-09-21 19:56:08 -0300

    cambiamos botones por checkbox

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ const filtroSexo = document.querySelectorAll("input[type='radio']")
 
 //armado de filtro por botones
 
-// for (let boton of botonesFiltro) {
-//     boton.onclick = () => {
-//         for (let card of cards) {
-//             if (boton.dataset.color === card.dataset.color) {
-//                 card.classList.remove(`hidden`)
-//             }
-//             else if (boton.dataset.color === "todos") {
-//                 card.classList.remove(`hidden`)
-//             }
-//             else {
-//                 card.classList.add(`hidden`)
-//             }
-//         }
-//     }
-// }
+for (let boton of botonesFiltro) {
+    boton.onclick = () => {
+        for (let card of cards) {
+            if (boton.dataset.color === card.dataset.color) {
+                card.classList.remove(`hidden`)
+            }
+            else if (boton.dataset.color === "todos") {
+                card.classList.remove(`hidden`)
+            }
+            else {
+                card.classList.add(`hidden`)
+            }
+        }
+    }
+}
 
 //armado de filtro por input
 
-// filtro.oninput = () =>{
-//     for (let card of cards){
-//         if(card.dataset.nombre.includes(filtro.value.toLowerCase())){
-//             card.classList.remove(`hidden`)
-//         }
-//         else {
-//             card.classList.add(`hidden`)
-//         }
-//     }
-// }
+filtro.oninput = () =>{
+    for (let card of cards){
+        if(card.dataset.nombre.includes(filtro.value.toLowerCase())){
+            card.classList.remove(`hidden`)
+        }
+        else {
+            card.classList.add(`hidden`)
+        }
+    }
+}
 
 
 //armado de filtro por checkbox o radio
